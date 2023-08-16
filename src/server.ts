0f12808a02152770477eea7e8dfe10d1718879fe
Eduard <eduard.liebenberger@streamliners.co.nz>
2023-08-16 07:02:43 +0000

    Added static frontend

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/api/health', (req: Request, res: Response) => {
     const data = 'ok';
